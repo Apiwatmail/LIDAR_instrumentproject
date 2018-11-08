2b5516f24eba1e29171ac2943e9a0f3be375960f
Joe Apiwat <atee1@student.monash.edu>
2018-11-08 15:14:07 +0100

    add class skeleton

--- main.m
+++ main.m
@@ -2,6 +2,37 @@
 
 %% variable
 % public var
-te
+trigger = input('Press any key to start the sytem'); 
+angle = 0; 
+servosGearing = 0; 
 
 %%class 
+function runMechnisms(angle)  
+topLever = 0;
+bottomLever = 0; 
+realTopSweep = topLever*servosGearing;
+realBottomSweep = bottomLever*servosGearing;
+
+    function rotateServos(angle)
+%         sweep
+    end
+
+    function lidarControl()
+%         tba
+    end
+
+end
+
+function analysis(terraingResponse) 
+flatPosi = [];
+slopePosi = [];
+subValLand = False; 
+totalLand = False; 
+
+    function terrainAna()
+%         tba
+    end
+    function landDecision()
+%         tba
+    end
+
