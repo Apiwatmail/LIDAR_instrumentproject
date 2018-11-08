d1b4a273646622ac58075654e9ac9fd996909ed9
Joe Apiwat <atee1@student.monash.edu>
2018-11-08 18:38:22 +0100

    update skeleton

--- main.m
+++ main.m
@@ -2,27 +2,38 @@
 
 %% variable
 % public var
-trigger = input('Press any key to start the sytem'); 
-angle = 0; 
-servosGearing = 0; 
+trigger = input('Press any key to start the system'); 
 
-%%class 
-function runMechnisms(angle)  
+board = arduino(port,board,{,}); 
+% lidar = ..;
+
+angle = 0; 
+servosGearing = 20/50; 
+scannedCount = 0; 
+
+
+%%Mechanism class 
+function runMechnisms(angle) 
 topLever = 0;
 bottomLever = 0; 
 realTopSweep = topLever*servosGearing;
 realBottomSweep = bottomLever*servosGearing;
 
     function rotateServos(angle)
+        coorInit = 1:250; %[position handle]
+        servoPosi = []; % [xCoor; yCoor] (based on angles)
+        matchSweep = [coorInit; servoPosi]; 
 %         sweep
-    end
+endtt
 
     function lidarControl()
+%         runLidar
 %         tba
     end
 
 end
 
+%% Analysis class
 function analysis(terraingResponse) 
 flatPosi = [];
 slopePosi = [];
@@ -30,9 +41,12 @@ subValLand = False;
 totalLand = False; 
 
     function terrainAna()
+        heat3DMap = heatmap(tbl,xvar,yvar);
 %         tba
     end
+
     function landDecision()
 %         tba
     end
 
+end
