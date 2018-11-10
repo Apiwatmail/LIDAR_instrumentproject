19fd41407bafc4aa152065a6cd0d5811e887d9f3
Joe Apiwat <atee1@student.monash.edu>
2018-11-10 16:42:27 +0100

    update mech part

--- main.m
+++ main.m
@@ -1,46 +1,65 @@
-% LiLa Project
+% LiLa LIDAR Landing Spacecraft Instrument Project
 
-%% variable
-% public var
-trigger = input('Press any key to start the system'); 
+% Written by the behalf of LILA Team
 
+% code structure: Object Oriented
+% all lengths are in the SI unit, and agles are in degrees
+
+%% Import Data 
 board = arduino(port,board,{,}); 
 % lidar = ..;
 
+%% variable
+% Public variable
+trigger = input('Press any key to start the system'); 
+scanDotResulotion = 50; 
+
+% Constant / Initial value 
 angle = 0; 
 servosGearing = 20/50; 
 scannedCount = 0; 
+initLoopVar = 1; 
 
-
-%%Mechanism class 
+%% Mechanism class 
 function runMechnisms(angle) 
 topLever = 0;
 bottomLever = 0; 
 realTopSweep = topLever*servosGearing;
-realBottomSweep = bottomLever*servosGearing;
+realBottomSweep = bottomLever*servosGearing; 
 
-    function rotateServos(angle)
-        coorInit = 1:250; %[position handle]
-        servoPosi = []; % [xCoor; yCoor] (based on angles)
-        matchSweep = [coorInit; servoPosi]; 
-%         sweep
-endtt
+dataCoor = ones(angle,angle,angle); %height x coor, y coor, height
+    
+    for i = initLoopVar:angle
+%         scan command here
+    end 
 
-    function lidarControl()
-%         runLidar
-%         tba
+    function scan(angle,lidarVal)
+        angle2length = 2*altitude*atan(angle); 
+        xservoPosi = linspace(-angle2length/2,angle2length/2); % [xCoor] (based on angles)
+        yservoPosi = linspace(-angle2length/2,angle2length/2); % [yCoor] (based on angles)
+        pushCoor = dataCoor(xservoPosi,yservoPosi); 
+        
+        matchSweep = dataCoor(:,:,lidarVal.height); 
     end
+
+% visualize
+figure1 
+heatmap(xServoPosi,yServoPosi,lidarVal.height); 
 
 end
 
 %% Analysis class
-function analysis(terraingResponse) 
+function analysis(terrainResponse) 
 flatPosi = [];
 slopePosi = [];
 subValLand = False; 
 totalLand = False; 
 
+
     function terrainAna()
+%         inclinX = diff(dataCoor..)/scanDotResulotion
+        inclinY = 
+        tooSteep = slope > 5 %in deg
         heat3DMap = heatmap(tbl,xvar,yvar);
 %         tba
     end
