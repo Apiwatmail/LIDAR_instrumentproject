ad36900297b03a755935c3002ec9c30f41f48236
Joe Apiwat <atee1@student.monash.edu>
2018-12-03 18:43:57 +0100

    Servo part done!

--- main.m
+++ main.m
@@ -5,25 +5,27 @@
 % code structure: Object Oriented Programming 
 % all distances are in the SI unit, and agles are in degrees
 
-clc,clear,close all 
+clc,clear all,close all 
 
 %% Import Data and set up devices 
 % board = arduino(sort,board,{real,real});
-% arduBoard = arduino();
-% s1=servo(arduBoard,'D7','MinPulseDuration',1000*10^-6,'MaxPulseDuration',2000*10^-6);
-% s2=servo(arduBoard,'D8','MinPulseDuration',1000*10^-6,'MaxPulseDuration',2000*10^-6);
+
+global arduBoard s1 s2
+arduBoard = arduino();
+s1=servo(arduBoard,'D7','MinPulseDuration',1000*10^-6,'MaxPulseDuration',2000*10^-6);
+s2=servo(arduBoard,'D8','MinPulseDuration',1000*10^-6,'MaxPulseDuration',2000*10^-6);
 
 % lidar = board.something..;
 
 %% variable
 % Public variable
-trigger = input('Press any key to start the system'); 
+% trigger = input('Press any key to start the system'); 
 
 global scanDotResulotion stepNum servosGearing topLever bottomLever scanAltitude 
 global angle scannedCount initLoopVar
 
-scanDotResulotion = 50; 
-stepNum = 125; 
+scanDotResulotion = 125; 
+stepNum = 10; 
 servosGearing = 50/20; 
 topLever = [0 1];
 bottomLever = [0 1]; 
@@ -35,90 +37,79 @@ scannedCount = 0;
 initLoopVar = 1; 
 
 %% Operational class 
-[dataCoor,arrangedTable] = runMechnisms(angle,scanAltitude); 
+heightData = runMechnisms(angle,scanAltitude); 
 % while scannedCount < 3
 %     [landingCoor,scannedCount] = analysis(terrainResponse); 
 % end
 
 %% Mechanism class 
-function [dataCoor,arrangedTable] = runMechnisms(angle,scanAltitude) 
-global servosGearing topLever bottomLever stepNum 
+function rawDataTable = runMechnisms(angle,scanAltitude) 
+global servosGearing topLever bottomLever stepNum s1 s2
     % create servo position table
-    dataCoor = ones(stepNum^2,3); %x coor, y coor, height
     angle2length = 2*scanAltitude*tand(angle(1)/2); 
 
     subcolPosi = angle2length(1); 
     countBase = linspace(1,subcolPosi,stepNum); flipBase = fliplr(countBase); 
     Posi = ones(stepNum,stepNum); 
     servoPosi = linspace(0,1,stepNum);
-    
+
     % send control to servos  
     realTopSweep = topLever*servosGearing;
     realBottomSweep = bottomLever*servosGearing; 
-    count = 1; 
+
     for r = 1:stepNum % create column position scan
+
         for c = 1:stepNum
             if rem(r,2) == 1
                 Posi(r,c) = countBase(c); 
                 writePosition(s1,servoPosi(r));
                 writePosition(s2,servoPosi(c));
-%                 dataCoor(count,3) = lidar.height; % get lidar altitude value 
-                count=count+1; 
-                pause(.01)
+%                 rawDataTable(r,c) = lidar.height; % get lidar altitude value 
+                pause(.001)
+                
             else
                 Posi(r,c) = flipBase(c); 
                 writePosition(s1,servoPosi(r));
-                writePosition(s2,servoPosi(c));
-%                 dataCoor(count,3) = lidar.height; % get lidar altitude value
-                count = count + 1; 
-                pause(.01) 
+                writePosition(s2,servoPosi(stepNum+1-c));
+%                 rawDataTable(r,c) = lidar.height; % get lidar altitude value 
+                pause(.001) 
             end
         end
     end
-    Posi = Posi'; Posi = Posi(:); dataCoor(:,2)=Posi;    
-    rowPosi = repmat(countBase,c,1); dataCoor(:,1) = rowPosi(:)';
-
-    % ==============================================
-
-arrangedTable = ones(125,125); 
-for localCount = 1:length(dataCoor)
-    arrangedTable(dataCoor(localCount,1),dataCoor(localCount,2)) = dataCoor(localCount,3)
-end
-    
+    rawDataTable = []; % get rid of this when lidar command's added 
 % visualize
-figure1 
-heatmap(dataCoor(:,1),dataCoor(:,2),dataCoor(:,3))
-
+% figure1 
+% heatmap(stepNum,stepNum,arrangedTable)
 end
 
 %% Analysis class
-function [landingCoor,scannedCount] = analysis(terrainResponse) 
-flatPosi = [];
-slopePosi = [];
-totalLand = False; 
-
-    function [terrainResponse,planeCheck] = terrainAna(terrainResponse)
-        planeCheck = abs(avg(surfnorm(terrainResponse))) <= 5; %in deg !Auchtung: the degree might need to be changed 
-         
-        landChar = gradient(terrainResponse); 
-        for rC = 1:length(arrangedTable) % stone check 
-            for cC = 1:length(arrangedTable)           
-                rockCheck = 0 <= abs(landChar(rC,cC)) && abs(landChar(rC,cC)) <= 5; %in deg !!Samma anteckning som planeCheck 
-                terrainResponse(rC,cC,2) = rockCheck(rC,cC); 
-            end 
-        end    
-    end
-
-    % identify the characteristics
-    edges = linspace(0,10,2); % Bin edges
-    labels = strcat({'Land'},{'Do not Land'}); % Labels for the bins
-    categorize = discretize(terrainResponse(:,:,2),'Categorical',labels);
-    group = grp2idx(categorize); 
-    idxStore = group == 1; 
-    landableStore = movsum(idxStore,5,'Endpoints','discard','omitnan'); 
-    
-    % pick data
-    % tba
-
-scannedCount = scannedCount + 1; 
-end
+% function [landingCoor,scannedCount] = analysis(terrainResponse) 
+% flatPosi = [];
+% slopePosi = [];
+% totalLand = False; 
+% 
+%     function [terrainResponse,planeCheck] = terrainAna(terrainResponse)
+%         planeCheck = abs(avg(surfnorm(terrainResponse))) <= 5; %in deg !Auchtung: the degree might need to be changed 
+%          
+%         landChar = gradient(terrainResponse); 
+%         for rC = 1:length(arrangedTable) % stone check 
+%             for cC = 1:length(arrangedTable)           
+%                 rockCheck = 0 <= abs(landChar(rC,cC)) && abs(landChar(rC,cC)) <= 5; %in deg !!Samma anteckning som planeCheck 
+%                 terrainResponse(rC,cC,2) = rockCheck(rC,cC); 
+%             end 
+%         end    
+%     end
+% 
+%     % identify the characteristics
+%     edges = linspace(0,10,2); % Bin edges
+%     labels = strcat({'Land'},{'Do not Land'}); % Labels for the bins
+%     categorize = discretize(terrainResponse(:,:,2),'Categorical',labels);
+%     group = grp2idx(categorize); 
+%     idxStore = group == 1; 
+%     landableStore = movsum(idxStore,5,'Endpoints','discard','omitnan'); 
+%     
+%     % pick data
+%     % tba
+% 
+% scannedCount = scannedCount + 1; 
+% end
